3f2dfb3d9f57865ec93956c3837c21d676576736
Richard Roncancio <batusai513@msn.com>
2016-07-07 17:25:56 -0500

    WIP

--- src/components/datetime.jsx
+++ src/components/datetime.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var valueMixin = require('./mixins/valueMixin');
 var multiMixin = require('./mixins/multiMixin');
 var DateTimePicker = require('react-widgets/lib/DateTimePicker');
+var DateTimeField = require('react-bootstrap-datetimepicker');
 var momentLocalizer = require('react-widgets/lib/localizers/moment');
 var moment = require('moment');
 momentLocalizer(moment);
@@ -15,18 +16,34 @@ module.exports = React.createClass({
   onChangeDatetime: function(index, value, str) {
     this.setValue(value, index);
   },
+  getMode: function(showCalendar, showTime){
+    if(showCalendar){
+      return 'date';
+    }else if(showTime){
+      return 'time';
+    }else if(showCalendar, showTime){
+      return undefined;
+    }
+  },
   getSingleElement: function(value, index) {
     return (
-      <DateTimePicker
-        id={this.props.component.key}
-        data-index={index}
-        name={this.props.name}
-        disabled={this.props.readOnly}
-        calendar={this.props.component.enableDate}
-        time={this.props.component.enableTime}
-        placeholder={this.props.component.placeholder}
-        value={value}
-        onChange={this.onChangeDatetime.bind(null, index)}
+      {/*<DateTimePicker
+              id={this.props.component.key}
+              data-index={index}
+              name={this.props.name}
+              disabled={this.props.readOnly}
+              calendar={this.props.component.enableDate}
+              time={this.props.component.enableTime}
+              placeholder={this.props.component.placeholder}
+              value={value}
+              onChange={this.onChangeDatetime.bind(null, index)}
+              />*/}
+        <DateTimeField
+          inputProps={{ id: this.props.component.key, name: this.props.name, disabled: this.props.readOnly }}
+          dateTime={value}
+          defaultText={this.props.component.placeholder}
+          mode={this.getMode(this.props.component.enableDate, this.props.component.enableTime)}
+          onChange={() => onChangeDatetime(index)}
         />
     );
   }
